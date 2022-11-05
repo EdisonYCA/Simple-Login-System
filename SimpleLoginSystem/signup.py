906d3a62111dd01395dd67d2ea580db8d27503be
EdisonYCA <edisonyca127@gmail.com>
2022-11-05 00:28:33 -0400

    TODO: fix main routine to allow user to login after signing up; vise versa.

--- SimpleLoginSystem/signup.py
+++ SimpleLoginSystem/signup.py
@@ -2,5 +2,5 @@ def add_user(database):
     # ask the user to enter new credentials
     new_username = input("Enter a username: ")
     new_password = input("Enter a password: ")
-
+    # stores user in database
     database[new_username] = new_password
