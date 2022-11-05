13f123fd84f4b8bd3a97397d73dee1cbd37b84a1
EdisonYCA <edisonyca127@gmail.com>
2022-11-04 23:51:13 -0400

    initial commit

---
+++ SimpleLoginSystem/login.py
@@ -0,0 +1,21 @@
+# Dictionary containing a list of user information
+database = {"edison1278": "123", "micheal1278": "124", "jeremy1278": "125"}
+
+# Ask the user to login in using username AND password
+user_username = input("Enter your username: ")
+user_password = input("Enter your password: ")
+
+
+# Search Dictionary for user credentials
+def validate_username(username, password):
+    for usernames in database:
+        if usernames == username:  # if users username matches a value in database
+            if password == database[username]:  # check if password matches the password for that username
+                print("Login granted.")
+                break
+        else:  # if username or password did not match
+            print("Invalid credentials.")
+            break
+
+
+validate_username(user_username, user_password)
