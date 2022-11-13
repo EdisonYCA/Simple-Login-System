831784b40835e6067a14438f43162e0d116a107d
EdisonYCA <edisonyca127@gmail.com>
2022-11-12 20:08:37 -0500

    "Re-wrote validate_username function to return True when correct username and password have been passed in. TODO: Fix 'none' being returned when called, change function name."

--- SimpleLoginSystem/login.py
+++ SimpleLoginSystem/login.py
@@ -7,19 +7,17 @@ def login_credentials():
     # Ask the user to login in using username AND password
     user_username = input("Enter your username: ")
     user_password = input("Enter your password: ")
-    validate_username(user_username, user_password)
+    validate_username(user_username, user_password, us.database)
 
 
 # Search Dictionary for user credentials
-def validate_username(username, password):
+def validate_username(username, password, database):
     # read current values in database
-    users = us.read_users()
-
-    for usernames in users:
+    for usernames in database:
+        print(f"Trying to match: {usernames} with {username}")
         if usernames == username:  # if users username matches a value in database
-            if password == users[username]:  # check if password matches the password for that username
-                print("Login granted.")
-                break
-        else:  # if username or password did not match
-            print("Invalid credentials.")
-            break
+            print(f"Found a match: {usernames} with {username}")
+            if password == database[username]:  # check if password matches the password for that username
+                print(f"Found a match: {password} with {database[username]}")
+                return True
+    return False
