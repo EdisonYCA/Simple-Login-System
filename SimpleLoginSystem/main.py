dc54fa0aa2a39ea00608825796f3dac4fa8bf02e
EdisonYCA <edisonyca127@gmail.com>
2022-11-12 20:09:52 -0500

    "Re-wrote main function to use a while-loop for signing up and logging in."

--- SimpleLoginSystem/main.py
+++ SimpleLoginSystem/main.py
@@ -1,18 +1,34 @@
-import login as log
 import signup
 import users
 
 
 def main():
-    request = input("Would you like to sign up or login: ")
-    if request == "login":
-        log.login_credentials()
-    elif request == "sign up":
-        signup.add_user(users.database)
-    else:
-        print("Please enter a valid option ('login' or 'sign up').")
+    run = True
+
+    print("Select one of the following options: \n" +
+          "1. Sign Up\n" +
+          "2. Login\n" +
+          "3. Exit\n")
+
+    while run:
+        request = input("--> ")
+
+        if request == "2":
+            pass
+        elif request == "1":
+            signup.add_user(users.database)
+        elif request == "3":
+            run = False
+        else:
+            print("Enter a valid option.")
+
+        print("Enter one of the following options to continue: \n" +
+              "1. Sign Up\n" +
+              "2. Login\n" +
+              "3. Exit\n")
 
 
 if __name__ == "__main__":
+    print("Initializing Login System..\n")
     main()
-    users.read_users()
+    print("Thank you!")
