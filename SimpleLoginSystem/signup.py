c342508ebd0d2d1218910140e6d46a029ac7dc86
EdisonYCA <edisonyca127@gmail.com>
2022-11-12 20:10:47 -0500

    "Temporarily removed pickling feature. Add pickle feature back when remaining of program is working."

--- SimpleLoginSystem/signup.py
+++ SimpleLoginSystem/signup.py
@@ -6,7 +6,9 @@ def add_user(database):
     new_username = input("Enter a username: ")
     new_password = input("Enter a password: ")
     # save database
-    users.store_users()
+    # users.store_users()
     # stores user in database
-    current_database = users.read_users()
-    current_database[new_username] = new_password
+    # current_database = users.read_users()
+    database[new_username] = new_password
+    print("Successfully signed up.\n")
+    print("New database: " + str(database) + "\n")
