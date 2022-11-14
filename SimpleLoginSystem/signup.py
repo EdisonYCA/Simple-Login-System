99d28011c0a804338a99a888ef22663750095ff0
EdisonYCA <edisonyca127@gmail.com>
2022-11-13 22:05:49 -0500

    "cleaned up 'add_user' function."

--- SimpleLoginSystem/signup.py
+++ SimpleLoginSystem/signup.py
@@ -5,10 +5,7 @@ def add_user(database):
     # ask the user to enter new credentials
     new_username = input("Enter a username: ")
     new_password = input("Enter a password: ")
-    # save database
-    # users.store_users()
-    # stores user in database
-    # current_database = users.read_users()
+    # save new credentials in database
     database[new_username] = new_password
     print("Successfully signed up.\n")
-    print("New database: " + str(database) + "\n")
+
