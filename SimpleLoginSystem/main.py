455c75e31f0968afa196c8bda52c08380d5d3239
EdisonYCA <edisonyca127@gmail.com>
2022-11-13 22:04:58 -0500

    "added condition for successful or unsuccessful login. added code to store users at exit of program and read users at beginning of program."

--- SimpleLoginSystem/main.py
+++ SimpleLoginSystem/main.py
@@ -1,5 +1,6 @@
 import signup
 import users
+import login
 
 
 def main():
@@ -14,15 +15,20 @@ def main():
         request = input("--> ")
 
         if request == "2":
-            pass
+            login_successful = login.validate_credentials(users.database)
+            if login_successful:
+                print("Login in successful.")
+            else:
+                print("Invalid credentials.")
         elif request == "1":
             signup.add_user(users.database)
         elif request == "3":
+            users.store_users()
             run = False
         else:
             print("Enter a valid option.")
 
-        print("Enter one of the following options to continue: \n" +
+        print("\nEnter one of the following options to continue: \n" +
               "1. Sign Up\n" +
               "2. Login\n" +
               "3. Exit\n")
@@ -30,5 +36,6 @@ def main():
 
 if __name__ == "__main__":
     print("Initializing Login System..\n")
+    users.database = users.read_users()
     main()
     print("Thank you!")
