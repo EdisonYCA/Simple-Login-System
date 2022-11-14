8dc5eac038c052d62d43c178bef239b039ae17da
EdisonYCA <edisonyca127@gmail.com>
2022-11-13 22:03:45 -0500

    "Removed 'login_credentials()' function and merged its code into 'validate_credentials.'"

--- SimpleLoginSystem/login.py
+++ SimpleLoginSystem/login.py
@@ -1,23 +1,17 @@
+import users
 import users as us
 import pickle
 
 
-# ask user to enter credentials and validate credentials
-def login_credentials():
+# Search database for user credentials and validate those credentials
+def validate_credentials(database):
     # Ask the user to login in using username AND password
-    user_username = input("Enter your username: ")
-    user_password = input("Enter your password: ")
-    validate_username(user_username, user_password, us.database)
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
 
-
-# Search Dictionary for user credentials
-def validate_username(username, password, database):
     # read current values in database
     for usernames in database:
-        print(f"Trying to match: {usernames} with {username}")
         if usernames == username:  # if users username matches a value in database
-            print(f"Found a match: {usernames} with {username}")
             if password == database[username]:  # check if password matches the password for that username
-                print(f"Found a match: {password} with {database[username]}")
                 return True
     return False
